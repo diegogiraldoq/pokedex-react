1e28d9d2f5b38e1cefc35bf8d4d06ca36a75bb27
Diego Alejandro <dgiraldoquintero50@gmail.com>
2022-03-02 20:51:55 -0500

    Create pages for MVP

---
+++ src/components/pages/PokemonDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DefaultLayout from '../layouts/DefoultLayout';
+
+/**
+ * Pokemon details component
+ * @returns {JSX.Element}
+ */
+export const PokemonDetails = () => (
+  <DefaultLayout>
+    <h1>Pokemon detail page</h1>
+</DefaultLayout>
+);
+
+export default PokemonDetails;
