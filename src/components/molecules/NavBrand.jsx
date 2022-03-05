7cb3ce607af8e05a6a594906c5c1ebb55249c47e
Diego Alejandro <dgiraldoquintero50@gmail.com>
2022-03-04 21:42:49 -0500

    Create dummy link for pokemon details page

--- src/components/molecules/NavBrand.jsx
+++ src/components/molecules/NavBrand.jsx
@@ -5,16 +5,16 @@ import { useNavigate } from "react-router-dom";
  * @returns {JSX.Element}
  */
 export const NavBrand = () => {
-  let navigate =useNavigate();
-  const handleClick= () => {
+  let navigate = useNavigate();
+  const handleClick = () => {
     navigate('/')
   };
   return(
-   <div oneClick={handleClick}>
-     <img src="" alt="Logo de la aplicacion"/>
+   <div onClick={handleClick}>
+     <img src='' alt='Logo de la aplicacion' />
      <span>POKEDEX_</span>
    </div>
- );
+  );
 };
 
 export default NavBrand;
