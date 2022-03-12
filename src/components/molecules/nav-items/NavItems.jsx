372b3f187e0728d25869eef7f1034237b6bbad4a
Diego Alejandro <dgiraldoquintero50@gmail.com>
2022-03-12 13:34:36 -0500

    Change text for about pages

--- src/components/molecules/nav-items/NavItems.jsx
+++ src/components/molecules/nav-items/NavItems.jsx
@@ -9,7 +9,8 @@ import './NavItems.css';
  */
 const NavItems = () => (
   <div className='m-nav.items'>
-    <Link className='m-nav-items__item' to='/about'>Acerca de</Link>
+    <Link className='m-nav-items__item' to='/about'>Información</Link>
+    
   </div>
 );
 
