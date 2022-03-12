490ad9574b4b0d8148cb349e0414cdb636d36d1d
Diego Alejandro <dgiraldoquintero50@gmail.com>
2022-03-12 09:31:32 -0500

    Replace relative for absolute paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Home from './components/pages//home/Home';
-import About from './components/pages/about/About';
-import PokemonDetails from './components/pages/pokemon-details/PokemonDetails';
-import NotFount from './components/pages/not-found/NotFount';
+import Home from 'components/pages//home/Home';
+import About from 'components/pages/about/About';
+import PokemonDetails from 'components/pages/pokemon-details/PokemonDetails';
+import NotFount from 'components/pages/not-found/NotFount';
 
 
 /**
