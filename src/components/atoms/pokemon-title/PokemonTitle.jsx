03f0485d717caf036876ca777314fd5c958f03dd
Diego Alejandro <dgiraldoquintero50@gmail.com>
2022-04-02 14:47:36 -0500

    Define style for pokemonTitle component

--- src/components/atoms/pokemon-title/PokemonTitle.jsx
+++ src/components/atoms/pokemon-title/PokemonTitle.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import './PokemonTitle.css';
 
+/**
+ * pokemon title component
+ * @returns {JSX.Element}
+ */
 const PokemonTitle = () => (
   <div className='a-pokemon-title'>
-    <img className='a-pokemon-title__image'
-    src='' 
-    alt='Imagen del tipo de elemento al cual pertenece el pokemon'
-    /> 
+    <div className='a-pokemon-title__image-mask'>
+      <img 
+        className='a-pokemon-title__image'
+        src='' 
+        alt='Imagen del tipo de elemento al cual pertenece el pokemon'
+      /> 
+    </div>
   <h1 className='a-pokemon-title__name'>Nombre del pokemon</h1>
   </div>
 );
+
 
 export default PokemonTitle;
